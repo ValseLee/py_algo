c1ae32921caaf99ce2cac17d871bd3e559cdad98
valseLee <sollleky72@gmail.com>
2022-05-25 21:16:12 +0900

    220525 Daily Study, py_algo, solved 2 on programmers

---
+++ test.py
@@ -0,0 +1,13 @@
+# it is my scribbles
+
+import sys
+sys.stdin = open("input.txt", "rt")
+
+def solution(array, commands):
+    answer = []
+    for i in len(commands):
+        l = commands[i][0]
+    print(l)
+        # answer.append()
+    print(answer)
+    return answer
